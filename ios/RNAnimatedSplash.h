539ef3269ca4796e0cc82df80a3a10ec030ae20c
William <william@capsens.eu>
2023-10-10 15:58:05 +0200

    trying to fix this duplication issue

--- ios/RNAnimatedSplash.h
+++ ios/RNAnimatedSplash.h
@@ -1,12 +1,8 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 #import "Animation/Splash.h"
+
 @interface RNAnimatedSplash : NSObject <RCTBridgeModule>
 @property (nonatomic, strong) UIWindow *window;
 
 @end
-  
+
